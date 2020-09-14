d1b39a699298b79b7baa6301231fc312c852f92d
philwilliammee <phil.williammee@gmail.com>
2020-09-14 11:54:55 -0400

    updating tests

--- tests/Templates.react.test.js
+++ tests/Templates.react.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 // Link.react.test.js
 import React from "react";
 import Standard from "../src/lib/js/components/molecules/standard";
@@ -5,8 +8,14 @@ import Compact from "../src/lib/js/components/molecules/compact";
 import ModernCompact from "../src/lib/js/components/molecules/modern_compact";
 import ModernStandard from "../src/lib/js/components/molecules/ModernStandard";
 import InlineCompact from "../src/lib/js/components/molecules/inline_compact";
+import Calendar from "../src/lib/js/components/molecules/EventsCalendar";
 import renderer from "react-test-renderer";
 import { componentData } from "./testData";
+
+beforeAll(() => {
+  window.VarA = "foo";
+  window.VarB = "bar";
+});
 
 test("Standard Enabled", () => {
   const component = renderer.create(
@@ -97,3 +106,27 @@ test("InlineCompact Enabled", () => {
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+/**
+ * TypeError: Cannot use 'in' operator to search for 'window' in null
+ * <MonthView> component: in MonthView (created by Calendar)
+ * @todo at least add some unit testing
+ */
+// test("Calendar Enabled", () => {
+//   const component = renderer.create(
+//     <Calendar
+//       heading="Calendar Test"
+//       events={componentData.events}
+//       filterby="none"
+//       wrapperClassArray={[]}
+//       listClassArray={[]}
+//       hidedescription="false"
+//       truncatedescription="250"
+//       hideimages="false"
+//       hideaddcal="true"
+//       hidepagination="true"
+//     />
+//   );
+//   let tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
