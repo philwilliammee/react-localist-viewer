b41fe04ab5b563490b6112be91eadb398b97f379
philwilliammee <phil.williammee@gmail.com>
2021-03-14 07:52:18 -0400

    added react query

--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import Localist from "./localist";
 import EventsState from "./js/context/EventState";
 import { AppProps } from "./types/types";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const App = (props: AppProps) => (
-  <EventsState>
-    <Localist {...props} />
-  </EventsState>
+  <QueryClientProvider client={queryClient}>
+    <EventsState>
+      <Localist {...props} />
+    </EventsState>
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>
 );
 
 export default App;
