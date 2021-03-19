7c3781c21b6e488144bf08fc7b45223dc9a5ce13
philwilliammee <phil.williammee@gmail.com>
2021-03-18 22:29:07 -0400

    Agenda Inner

---
+++ src/stories/AgendaInnerContent.stories.tsx
@@ -0,0 +1,31 @@
+import React, { ComponentProps } from "react";
+import { Story, Meta } from "@storybook/react";
+import { singleEvent } from "./assets/testData";
+import { EventEvent } from "lib/types/types";
+import AgendaInnerContent from "lib/js/components/molecules/EventsCalendar/AgendaList/AgendaInnerContent";
+
+const AgendaInnerContentStories = {
+  title: "Molecules/AgendaInnerContent",
+  component: AgendaInnerContent,
+} as Meta;
+
+// @todo fix this omg this is ugly. Why pass the whole event?
+const Template: Story<ComponentProps<typeof AgendaInnerContent>> = (args) => (
+  <div className="cwd-events-style">
+    <div className="events-modern-standard ">
+      <div className="cwd-component">
+        <div className="events">
+          <AgendaInnerContent {...args} />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const eventData: EventEvent = { ...singleEvent.event };
+export const Default = Template.bind({});
+Default.args = {
+  event: eventData,
+};
+
+export default AgendaInnerContentStories;
